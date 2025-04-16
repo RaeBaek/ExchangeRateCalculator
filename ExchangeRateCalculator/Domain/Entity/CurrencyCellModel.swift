3295293faee073b00fe164321e6342a789d58618
RaeBaek <bmp117@naver.com>
2025-04-16 10:48:39 +0900

    refactor: 파일 및 폴더 구조 수정
    - CurrencyCellModel 도메인 모델 추가 (code, name, rate 정보 포함)
    - MainTableViewCell → View/Cell 폴더로 이동 및 파일 정리
    - MainView, MainViewController, MainViewModel 각각 View / ViewController / ViewModel 디렉토리로 분리
    - Extension+UITableViewCell, ReusableProtocol → Shared 디렉토리로 이동

---
+++ ExchangeRateCalculator/Domain/Entity/CurrencyCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyCellModel.swift
+//  ExchangeRateCalculator
+//
+//  Created by 백래훈 on 4/16/25.
+//
+
+import Foundation
+
+struct CurrencyCellModel {
+    let code: String
+    let name: String
+    let rate: String
+}
